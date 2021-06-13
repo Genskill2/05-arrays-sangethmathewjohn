76efff5b7eb983c855be56f5f6877d44f2dda2e7
Sangeeth Mathew John <65032296+sangethmathewjohn@users.noreply.github.com>
2021-06-13 18:27:18 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,32 +1,33 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
-
+/********----Average----********/
 float average(int a[],int num){
-  int i;
-  float sum=0;
+  int i, sum=0;
   for (i=0;i<=num;i++){
     sum += a[i];
   }
-  sum/=num;
-  return (sum);
+  sum=sum/num;
+  return sum;
 }
-
+/******---Maximum---******/
 int max(int a[],int num){
   int i,max= 0;
   for (i=0;i<=num;i++){
-    if(max < a[i])
+    if(max < a[i]){
      max=a[i];
+    } 
   }
-  return (max);
+  return max;
 }
-
+/******---Minimum---******/
 int min(int a[],int num){
   int i=0,min=a[i];
   for(i;i<=num;i++){
-    if(a[i]<min)
+    if(a[i]<min){
      min=a[i];
+    }
   }
-  return(min);
+  return min;
 }
 
 int mode(int a[],int num){
